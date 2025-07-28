c95019485b0f15f8c194beff851bd3533f68fa89
eeshan2005 <ajarekareeshan@gmail.com>
2025-07-28 21:10:19 +0530

    refresh issue

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kubuqoazrafjhsdeffny.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1YnVxb2F6cmFmamhzZGVmZm55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxNTgzNDQsImV4cCI6MjA2ODczNDM0NH0.31oJhLNctzlY9mEo6g_O0_ObXwAnMu7qg26a2rn6gd4';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 // Create Supabase client with explicit auth configuration
 const supabase = createClient(supabaseUrl, supabaseAnonKey, {
